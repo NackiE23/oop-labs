f0b519c7f3c21fcdedaa52eb0309f076169d78b9
NackiE23 <dimavarchuc@gmail.com>
2023-11-27 20:00:49 +0200

    Lab3 (Not finished)

---
+++ lab3/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿System.Console.WriteLine("Enter array size: ");
+int size = int.Parse(Console.ReadLine()!);
+
+double minimum = -2.9;
+double maximum = 60.3;
+
+double[] array =new double[size];
+
+Random random = new Random();
+for (int i = 0; i < size; i++)
+{
+    array[i] = Math.Round(random.NextDouble() * (maximum - minimum) + minimum, 1);
+}
+
+System.Console.WriteLine("Array: " + string.Join(", ", array));
+
+// Find Min/Max Values
+int minIndex = 0, maxIndex = 0;
+double minValue = array[0], maxValue = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] < minValue)
+    {
+        minValue = array[i];
+        minIndex = i;
+    }
+    else if (array[i] > maxValue)
+    {
+        maxValue = array[i];
+        maxIndex = i;
+    }
+}
+System.Console.WriteLine($"minIndex = {minIndex}, maxIndex = {maxIndex}");
+
+int start, end;
+if (minIndex > maxIndex)
+{
+    start = maxIndex;
+    end = minIndex;
+}
+else
+{
+    start = minIndex;
+    end = maxIndex;
+}
+System.Console.WriteLine($"start = {start}, end = {end}");
+
+int product = 1;
+for (int i = start + 1; i < end; i++)
+{
+    product *= i;
+}
+
+System.Console.WriteLine($"Product: {product}");
